9c8e4dda8ef765e9f7fd0c8a1a61d983a63612c6
jacobbien <jbien@usc.edu>
2026-03-04 12:38:40 -0800

    make legend optional

--- randhclust/R/plot_clustered_data.R
+++ randhclust/R/plot_clustered_data.R
@@ -10,12 +10,14 @@
 #' @param clust2 n-vector of cluster indices (optional)
 #' @param clust2_label name of clustering 2 (e.g. "estimated clustering"); optional
 plot_clustered_data <- function(x, clust1, clust1_label = "clust1", clust2 = NULL, clust2_label = "clust2") {
+  no_legend <- is.null(clust1_label)
+  if (no_legend) clust1_label <- "clust1"
   dat <- tibble::tibble(x1 = x[, 1], x2 = x[, 2], clust1 = clust1)
   dat <- dat %>% 
     dplyr::mutate(!!clust1_label := paste0("clust", clust1))
   if (is.null(clust2)) {
     # show only one clustering
-    dat %>% 
+    p <- dat %>% 
       ggplot2::ggplot(ggplot2::aes(x = .data$x1,
                                    y = .data$x2,
                                    color = .data[[clust1_label]])) + 
@@ -26,11 +28,13 @@ plot_clustered_data <- function(x, clust1, clust1_label = "clust1", clust2 = NUL
     # show two clusterings
     dat <- dat %>% 
       dplyr::mutate(!!clust2_label := paste0("clust", clust2))
-    dat %>% 
+    p <- dat %>% 
       ggplot2::ggplot(ggplot2::aes(x = .data$x1, y = .data$x2,
                  color = .data[[clust1_label]],
                  shape = .data[[clust2_label]])) + 
       ggplot2::geom_point() +
       ggplot2::coord_equal()
   }
+  if (no_legend) p <- p + ggplot2::theme(legend.position = "none")
+  return(p)
 }
